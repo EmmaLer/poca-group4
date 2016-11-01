cc9d87c02217c2f8df57a562f44a84834bbd16e7
DECERLE <elodie.decerle@etu.univ-paris-diderot.fr>
2016-11-01 20:56:51 +0100

    Ajout de la classe minion

---
+++ version0/src/Minion.scala
@@ -0,0 +1,10 @@
+/**
+ * classe de définition des Minions
+ * auteur : Elodie DECERLE
+ */
+
+class Minion (var niveauDeConnerie: Int,
+    var prenom: String, var joueur: Joueur, 
+    var position : Position) {
+  
+}
