33c5b5bdb22c07cd5d45a43543b9aa7369e62e21
Vannida <elodec@orange.fr>
2016-11-03 16:08:28 +0100

    test pour affichage contribution

--- version0/src/Minion.scala
+++ version0/src/Minion.scala
@@ -7,4 +7,6 @@ class Minion (niveauDeConnerie: Int,
     prenom: String, proprietaire : Joueur,
     position : Position) {
     
+  /* mettre une méthode pour posséder une arme?*/
+  /* mettre les getters et les setters */
 }
