605d991c9a07fc3e0a078b473268b57c71e865a6
Vannida <elodec@orange.fr>
2016-11-07 23:18:26 +0100

    ajout des signatures des méthodes

--- Version_0/src/Minion.scala
+++ Version_0/src/Minion.scala
@@ -3,10 +3,43 @@
  * auteur : Elodie DECERLE
  */
 
+package minion
+
+import position.Position
+import joueur.Joueur
+/*
+ * comment faire l'import ? et utiliser 
+ * les classes Joueur et Position comme arguments du
+ * constructeur
+ */
+
+
+
 class Minion (niveauDeConnerie: Int,
-    prenom: String, proprietaire : Joueur,
+    prenom: String, proprietaire :  Joueur,
     position : Position) {
     
-  /* mettre une méthode pour posséder une arme?*/
-  /* mettre les getters et les setters */
+  /* avec la potion, le minion modifie son niveau
+   * de connerie
+   */
+  def modifieConnerie(gain:Int): Int = {
+    var niveauDeConnerie : Int = niveauDeConnerie + gain
+    return niveauDeConnerie
+  }
+  
+  /*
+   * connaitre le propriétaire du minion
+   */
+  def getProprio(): String = {
+    var pseudo : String = "toto"
+    return pseudo;
+  }
+  
+  /* 
+   * connaitre la position du minion
+   */
+  def getPositionMinion(): Position = {
+    
+  }
+  
 }
