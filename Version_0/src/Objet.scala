390a81770d8b43e1382896a7968fad1f1fbc6cc4
samiaksabi <samia.ksabi@gmail.com>
2016-11-08 22:19:56 +0100

    squelette de la classe Objet

--- Version_0/src/Objet.scala
+++ Version_0/src/Objet.scala
@@ -1,36 +1,60 @@
+/* Classe des definition des objet */
+
+
 
 package objet 
 
 import joueur.Joueur
 import minion.Minion 
 import position.Position 
+import carte.Carte 
 
-class Objet {
+abstract class Objet {
  var position : Position
 
+ 
 
 def getPositionObjet(): Position = { 
    var p : Position = position
 return p;
 }
+ 
+ 
+def creerObjet( obj : Objet, p : Position ){
+  obj.position = p
+}
+
+/** verification de la methode remove **/
+def existe(p : Position): Boolean = {
+  if(position == p){
+    return true
+  }
+return false
+}
 
 
-def remove(){
-  position = joeur.position 
+def remove() = {
+  position = joueur.position 
+}
+}
+
+class Banana(p:Position, gain : Int) extends Objet ( p ){
+  val gain = scala.util.Random
   
-}
-}
-
-abstract class Banana(p:Position) extends Objet ( p ){
-  gain = 10
-   def captured ()={
-    if ( joueur.position == minion.position ){
+  def getGain () : Int = { 
+  var  g : Int = 0
+   g = gain.nextInt(20)
+   return g
+  }
+   def captured ()={}
+ /**   if ( joueur.position = minion.position ){
     banana : Banana = new Banana 
     joeur.add(banana)
     minion.modifieConnerie(10)
     banana.delete() 
     }
   }
+  **/
   
 }
 
@@ -38,4 +62,5 @@ abstract class Banana(p:Position) extends Objet ( p ){
 
 
 def main(args: Array[String]) {
+  var b : Banana = new 
 }
