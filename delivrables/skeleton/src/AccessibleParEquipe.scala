a4e8b7053e4b525015a460c06b39fd88b6c87168
emma <emma.lerandy@gmail.com>
2016-11-29 11:03:16 +0100

    Delete skeleton

--- delivrables/skeleton/src/AccessibleParEquipe.scala
+++
@@ -1,5 +0,0 @@
-package accessibleParEquipe
-
-trait AccessibleParEquipe {
-  
-}
--- delivrables/skeleton/src/Lieu.scala
+++
@@ -1,9 +0,0 @@
-/** abstract class Lieu qui permet de définir les endroits accessibles par les joueurs **/
-
-package lieu
-
-import position.Position
-
-abstract class Lieu (var position : Position) {
-  // TODO : donner la possibilite de faire des tableaux de positions pour gerer la superficie des lieux
-}
