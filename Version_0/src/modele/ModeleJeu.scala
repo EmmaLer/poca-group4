a0cbd27fe50bbffb7c08bd4fbabbb29ad26903ee
emma <emma.lerandy@gmail.com>
2016-11-29 12:23:22 +0100

    Test OK

--- Version_0/src/modele/ModeleJeu.scala
+++ Version_0/src/modele/ModeleJeu.scala
@@ -9,25 +9,29 @@ class ModeleJeu (var sizeX:Int,var sizeY:Int){
   
   def this(){
     this(5,5);
+    sizeX = 5;
+    sizeY = 5;
     println("Lancement MinionGame !");
     plateau(sizeX,sizeY);
     
   }
   
-  //Création de la carte
+  //Création de la carte -- Créer une classe Carte
   def plateau (x:Int, y:Int){ 
     var p = ofDim[Int](x,y);
     var i,j=0;
-    for(i <- 0 to x){
-      for(j <- 0 to y){
+    for(i <- 0 to x-1){
+      for(j <- 0 to y-1){
         p(i)(j) = 0;
       }
     }
-    for (i<- 0 to x){
-      for(j<-0 to y)
-        println(p(i)(j));
-      println("/n");
+    for (i<- 0 to x-1){
+      for(j<-0 to y-1)
+        print(p(i)(j)+ "|");
+      println();
     }
   }
+  
+  //
   
 }
