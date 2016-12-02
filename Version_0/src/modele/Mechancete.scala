84d6dc3fbdc299254b49bd6620de19bf1bc4ada0
Vannida <elodec@orange.fr>
2016-12-02 21:57:14 +0100

    ajout de la trait Mechancete

---
+++ Version_0/src/modele/Mechancete.scala
@@ -0,0 +1,12 @@
+package modele
+
+/**
+ * definit les methodes effectues par les entites du 
+ * jeu qu'ils soient Joueur ou Minion en fonction de 
+ * leur mechancete
+ */
+
+trait Mechancete {
+  def aiderProprio
+  def nuireProprio
+}
--- Version_0/src/modele/MinionMechant.scala
+++
@@ -1,9 +0,0 @@
-package modele
-import controleur.AbstractCoordonnees
-
-
-class MinionMechant(niveauDeConnerie: Int, prenom: String,  position : AbstractCoordonnees)
-  extends Minion(niveauDeConnerie: Int, prenom: String, position : AbstractCoordonnees) {
- 
-  def nuireProprio : Unit = { println("méthode à implémenter")}
-}
