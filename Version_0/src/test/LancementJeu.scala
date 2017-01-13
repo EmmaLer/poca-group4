e78d65390a51a02d6a20434fffadff125c16ced8
emma <emma.lerandy@gmail.com>
2017-01-13 10:58:34 +0100

    LancementJeu / PanelJeu

--- Version_0/src/test/LancementJeu.scala
+++ Version_0/src/test/LancementJeu.scala
@@ -4,31 +4,38 @@ import scala.collection.mutable.ListBuffer
 import controleur.ControleurJeu
 import modele.ModeleJeu
 import vue.VueJeu
-
+import scala.swing._
 
 class LancementJeu (modele: ModeleJeu, controleur: ControleurJeu) extends Observateur {
   var listeObs = new ListBuffer [Observateur]();
+  var panel: Panel = new GridPanel(5,5)
   
   def lancement (){
-    
-    
+    var vue = new VueJeu(controleur)
+    modele.ajouterObs(vue)
+    panel = vue.frame.canvas
   }
   
   def miseAjour() {
     val controleur = new ControleurJeu(modele)
-    var jeu:VueJeu=new VueJeu(controleur)
+    var jeu =new VueJeu(controleur)
     modele.ajouterObs(jeu)
     modele.prevenirObs()
   }
  
   def ajouterObs(o:Observateur){
-    
+    listeObs.+=(o)
   }
   
   def supprimerObs (){
-    
+    listeObs = new ListBuffer[Observateur]()
   }
+  
   def prevenirObs(){
+    for(o <- listeObs) o.miseAjour()
+  }
+  
+  class PanelJeu () extends Panel {
     
   }
 }
