682ed658b4fa01ecabd8b8ca18f958c10e2915d9
Vannida <elodec@orange.fr>
2017-01-16 17:27:10 +0100

    utilisation de la méthode caseAtteignable boolean qui fusionne mvtAtteignable mvtPossible

--- Version_0/src/controleur/ControleurJeu.scala
+++ Version_0/src/controleur/ControleurJeu.scala
@@ -3,6 +3,7 @@ package controleur
 
 import modele.ModeleJeu
 import modele.Joueur
+import modele.ZoneCarre
 import scala.collection.mutable.ListBuffer
 
 class ControleurJeu (var modele: ModeleJeu){
@@ -47,11 +48,24 @@ class ControleurJeu (var modele: ModeleJeu){
 
     return l
   }
+
+  def caseAtteignable(caseArrivee: Coordonnees2D): Boolean = {
+    return ((caseArrivee.x == modele.joueur.position.x + 1 && caseArrivee.y == modele.joueur.position.y)
+      || (caseArrivee.x == modele.joueur.position.x - 1 && caseArrivee.y == modele.joueur.position.y)
+      || (caseArrivee.x ==modele.joueur.position.x && caseArrivee.y ==  modele.joueur.position.y + 1)
+      || (caseArrivee.x ==modele.joueur.position.x && caseArrivee.y ==  modele.joueur.position.y - 1)
+      || (caseArrivee.x ==modele.joueur.position.x + 1 && caseArrivee.y ==  modele.joueur.position.y + 1)
+      || (caseArrivee.x ==modele.joueur.position.x - 1 && caseArrivee.y ==  modele.joueur.position.y + 1)
+      || (caseArrivee.x ==modele.joueur.position.x - 1 && caseArrivee.y ==  modele.joueur.position.y - 1)
+      || (caseArrivee.x ==modele.joueur.position.x + 1 && caseArrivee.y ==  modele.joueur.position.y - 1)
+      || (caseArrivee.x ==modele.joueur.position.x && caseArrivee.y ==  modele.joueur.position.y))
+  }
     
     // si le mvt est modele.joueur.position est possible on fait le déplacement 
   // sinon retour à la case d'avant
   def control (pos2: Coordonnees2D)={ 
-    if (mvtPossible(pos2)){
+    //if (mvtPossible(pos2)){
+    if (caseAtteignable(pos2)) {
       println("Mouvement possible !!" + modele.joueur.position + "arrive" + pos2);
     // je fais le mvt dans le modele
       modele.deplacement(pos2);
